8a47776af4b06da3e7b7fb48a429bd07c2a05e96
Tanner Noble <tannernoble@gmail.com>
2018-09-28 00:57:01 -0500

    built my first funcional component

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Tnobler Madlibs</h1>
+      <p>Tnobler Madlibs', 'Fill out the fields below and click the generate button to see the Mad Lib story.</p>
+    </div>
+  )
+}
+
+export default Header;
